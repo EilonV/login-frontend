ec61463651dcc58ebc9aa739d986abac31bb2279
EilonV <73593619+EilonV@users.noreply.github.com>
2023-07-25 13:55:53 +0300

    basic form design

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,22 @@
 export const Login = () => {
-    return <section>
-        <p>login</p>
+    const handeForm = (e) => {
+        e.preventDefault()
+    }
+    return <section className="home flex column align-center justify-center">
+        <div className="gradient" />
+        <div className="login">
+            <h1>Login</h1>
+            <form className="flex column" action="/form" autoComplete="new-password" onSubmit={handeForm}>
+                <input type="text" autoComplete="new-password" />
+                <input type="password" autoComplete="new-password" />
+                <button>Sign in</button>
+            </form>
+            <div className="color-line-wrapper flex">
+                <div className="color-line line1" />
+                <div className="color-line line2" />
+                <div className="color-line line3" />
+                <div className="color-line line4" />
+            </div>
+        </div>
     </section>
 }
--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { Login } from "./pages/login";
+import './assets/scss/styles.scss'
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" Component={Login} />
+      <Route path="/" Component={Login} />
     </Routes>
   );
 }
